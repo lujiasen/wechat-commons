9c70cdc9f672505ef55556b422e8f2a6c6ccf9d1
lujs <919289699@qq.com>
2018-03-31 10:59:36 +0800

    修改

--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,5 @@
-const URL = "http://192.168.1.165:8080/";
+//const URL = "http://192.168.1.165:8080/";
+const URL = "http://www.lujiasen.com/wechat/"; //正式线上地址
 const EXPRESS_INFORMATION = URL+"express/getExpressState";
 
 const getParams = url =>{
